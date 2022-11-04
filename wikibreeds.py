c63cfd71fe0846305f15b804d27010bdd5ff0c5e
HM Rando <halie.rando@pennmedicine.upenn.edu>
2022-11-04 13:55:15 -0400

    add letters after C + exitinct

--- wikibreeds.py
+++ wikibreeds.py
@@ -1,19 +1,16 @@
-import os
-import pandas as pd
-import spacy
 from scrapeFunctions import retrieve_html
-from bs4 import BeautifulSoup
 import csv
 
 
 pageContent = retrieve_html("https://en.wikipedia.org/wiki/List_of_dog_breeds")
-breedgroupInfo = pageContent.find("div", {"class": "div-col"})
 breeds = list()
-for litag in breedgroupInfo.find_all('li'):
-    linkInfo = litag.find(href=True)
-    breed = linkInfo.text
-    url = "https://en.wikipedia.org" + linkInfo['href']
-    breeds.append([breed, url])
+breedgroupInfo = pageContent.find_all("div", {"class": "div-col"})
+for metacategory in breedgroupInfo:
+    for litag in metacategory.find_all('li'):
+        linkInfo = litag.find(href=True)
+        breed = linkInfo.text
+        url = "https://en.wikipedia.org" + linkInfo['href']
+        breeds.append([breed, url])
 
 with open("rawdata/wiki.csv", "w",  newline="") as f:
     writer = csv.writer(f)
